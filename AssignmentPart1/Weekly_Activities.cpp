81bdb5431694bb5b77e1c958c0a54e97103edd07
Edward <62472600+horbosis556@users.noreply.github.com>
2021-02-19 23:53:30 +0700

    Add files via upload

---
+++ AssignmentPart1/Weekly_Activities.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    string day_input;
+    vector<string>all_days{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    // Input Validation Is Not Necessarily Required In This Task.
+    cout << "What day is it today?" << endl;
+	cin >> day_input;
+
+    /*
+        If Input Is Found In String Vector:
+            If The Index Of Found Vector Element Is Less Than Indexes For Elements "Saturday" & "Sunday", Then Input Is A WeekDay:
+                Else, Input Is A WeekEnd Day.
+    */
+    for(int i = 0; i < all_days.size(); i++){
+        if(all_days[i] == day_input){
+            if(i < 5){
+                cout << "I Will Study." << endl;
+            }
+            else{
+                cout << "I Will Take A Break And Go Catch Some Fish." << endl;
+            }
+        }
+    }
+}
