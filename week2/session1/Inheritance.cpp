49e326d4a66b921707cf1dbe2032f1b21e357e9f
Tinky Winky <62472600+TinkyWinky556@users.noreply.github.com>
2021-03-02 12:08:23 +0700

    Add files via upload

---
+++ week2/session1/Inheritance.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+//Create a Shape class which has width and height data members.
+class Shape{
+    protected:
+    int height;
+    int width;
+
+    public:
+    void set_width(double w){
+        width = w;
+    }
+
+    void set_height(double h){
+        height = h;
+    }
+
+    double get_width(){
+        return width;
+    }
+
+    double get_height(){
+        return height;
+    }
+
+    // Create a member function to calculate area.
+    double get_area(){
+        return height * width;
+    }
+
+};
+
+// Create a sub class Rectangle which inherits the Shape class.
+class Rectangle: public Shape{
+    // Override the function to calculate area.
+    public:
+    double get_area(){
+        return height * width;
+    }
+};
+
+int main(){
+    // In the main function. create objects of a derived class.
+    Rectangle MyObj;
+
+    // Access its member data and invoke its methods.
+    MyObj.set_height(32.4);
+    MyObj.set_width(29.5);
+
+    // Access Overridden Method In Rectangle Class.
+    Rectangle* ptr = &MyObj;
+    cout << ptr->get_area();
+
+    return 0;
+}
+
